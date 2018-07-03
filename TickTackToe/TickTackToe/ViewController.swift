dc902567a7a017d1f455814771fbac9faa1a43b6
jupterorbita <john.williams@outlook.com>
2018-07-03 12:19:16 -0700

    init

---
+++ TickTackToe/TickTackToe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TickTackToe
+//
+//  Created by J on 7/03/2018.
+//  Copyright © 2018 J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
